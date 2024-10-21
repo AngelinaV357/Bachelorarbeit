f483daa52d771c9af434591c6bb1c0fda5adbc47
AngelinaV357 <Angelina.viessmann@uni-bayreuth.de>
2024-10-21 11:28:14 +0200

    XMLReader angepasst mit Konsolenausgabe

--- src/main/java/com/example/XMLReader.java
+++ src/main/java/com/example/XMLReader.java
@@ -1,71 +1,34 @@
 package com.example;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.camunda.bpm.model.bpmn.Bpmn;
 import org.camunda.bpm.model.bpmn.BpmnModelInstance;
+import org.camunda.bpm.model.bpmn.instance.Activity;
 import org.camunda.bpm.model.bpmn.instance.FlowNode;
 import org.camunda.bpm.model.bpmn.instance.SequenceFlow;
-import org.camunda.bpm.model.bpmn.instance.StartEvent;
-import org.camunda.bpm.model.bpmn.instance.UserTask;
 
 public class XMLReader {
-
     public static void main(String[] args) {
-        // BPMN-Modell von Datei lesen
-        BpmnModelInstance modelInstance = Bpmn.readModelFromFile(new File("C:\\Users\\pinki\\OneDrive\\Desktop\\Bachelor\\XML Dateien\\Car Wash Process.bpmn"));
+        File file = new File("C:\\Users\\pinki\\OneDrive\\Desktop\\Bachelor\\XML Dateien\\May_combine_ingredients.bpmn");
+        
+        // Read the BPMN model instance from the file
+        BpmnModelInstance modelInstance = Bpmn.readModelFromFile(file);
+        System.out.println("BPMN-Datei erfolgreich eingelesen!");
 
-        // Start-Event finden
-        StartEvent startEvent = (StartEvent) modelInstance.getModelElementById("start");
-        if (startEvent != null) {
-            System.out.println("Start Event ID: " + startEvent.getId());
-            System.out.println("Start Event Name: " + startEvent.getName());
-
-            // Attribute bearbeiten
-            startEvent.setId("new-start-id");
-            startEvent.setName("New Start Event Name");
-
-            // Änderungen anzeigen
-            System.out.println("Updated Start Event ID: " + startEvent.getId());
-            System.out.println("Updated Start Event Name: " + startEvent.getName());
-        }
-
-        // Alle Aufgaben finden
-        Collection<UserTask> userTasks = modelInstance.getModelElementsByType(UserTask.class);
-        for (UserTask task : userTasks) {
-            System.out.println("User Task ID: " + task.getId());
-        }
-
-        // Sequenzfluss finden und Quell- und Zielelemente abrufen
-        SequenceFlow sequenceFlow = (SequenceFlow) modelInstance.getModelElementById("start-task1");
-        if (sequenceFlow != null) {
-            FlowNode source = sequenceFlow.getSource();
-            FlowNode target = sequenceFlow.getTarget();
-            System.out.println("Source ID: " + source.getId());
-            System.out.println("Target ID: " + target.getId());
-
-            // Alle ausgehenden Sequenzflüsse des Quellknotens abrufen
-            Collection<SequenceFlow> outgoingFlows = source.getOutgoing();
-            for (SequenceFlow flow : outgoingFlows) {
-                System.out.println("Outgoing Flow ID: " + flow.getId());
+        // Find all sequence flows
+        Collection<SequenceFlow> sequenceFlows = modelInstance.getModelElementsByType(SequenceFlow.class);
+        
+        // Iterate through sequence flows and print connections
+        for (SequenceFlow flow : sequenceFlows) {
+            FlowNode source = flow.getSource();
+            FlowNode target = flow.getTarget();
+            if (source instanceof Activity && target instanceof Activity) {
+                String sourceName = ((Activity) source).getName();
+                String targetName = ((Activity) target).getName();
+                System.out.println("\"" + sourceName + "\" ist mit \"" + targetName + "\" verbunden.");
             }
         }
-
-        // Beispiel für die Verwendung einer Hilfsmethode
-        Collection<FlowNode> followingNodes = getFlowingFlowNodes(startEvent);
-        for (FlowNode node : followingNodes) {
-            System.out.println("Following Node ID: " + node.getId());
-        }
-    }
-
-    // Hilfsmethode, um die folgenden Flussknoten zu erhalten
-    public static Collection<FlowNode> getFlowingFlowNodes(FlowNode node) {
-        Collection<FlowNode> followingFlowNodes = new ArrayList<>();
-        for (SequenceFlow sequenceFlow : node.getOutgoing()) {
-            followingFlowNodes.add(sequenceFlow.getTarget());
-        }
-        return followingFlowNodes;
     }
 }
